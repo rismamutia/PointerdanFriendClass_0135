a4e739281c91e09a775a46f6f417eec0cf7b27b0
rismamutia <rismamutiadewi89@gmail.com>
2025-05-06 18:29:34 +0700

    mendeklarasikan variabel

--- contoh.cpp
+++ contoh.cpp
@@ -6,6 +6,17 @@ class PersegiPanjang
     public: // akses modifier
         int panjang, lebar;
     
-   
+        public:
+        void input()
+        {
+            cout << "Masukkan panjang : ";
+            cin >> panjang;
+            cout << "Masukkan lebar : ";
+            cin >> lebar;
+        }
     
+        int luasPp(int a, int b)
+        {
+            return a * b;
+        }
 }
